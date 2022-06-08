0d7a6ed09065a8e9fff4bb944b31d6c4115cdfe2
MuqeetEmumba <muhammad.muqeet@emumba.com>
2022-06-08 22:25:13 +0500

    Third Chapter

---
+++ 04-functions.js
@@ -0,0 +1,27 @@
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(10, 10));
+
+function multiplication(a, b) {
+    return a * b;
+}
+console.log(multiplication(10, 10));
+
+const storeValue = multiplication(15, 6);
+
+console.log(storeValue);
+
+const valueNumber = sum(20, 4);
+
+console.log(valueNumber);
+
+function division(a, b) {
+    return a / b;
+}
+console.log(division(15, 5));
+
+const valueDivision = division(25, 5);
+
+console.log(valueDivision);
